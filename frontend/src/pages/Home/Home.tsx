d52f3bfead6685a3fd4380a9550c070b228fe8cb
Jérémy <jeremy.lauret@eleves.enpc.fr>
2019-04-30 12:57:10 +0200

    Create Pokemon component with name and id.

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,19 +2,15 @@ import * as React from 'react';
 
 import Style from './Home.style';
 
+import Pokemon from 'components/Pokemon';
+
 class Home extends React.Component {
   render(): React.ReactNode {
-    const pokemon = 'Carapuce';
-    const pokemon_number = 7;
-    const pokemon_image_url =
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png';
-
     return (
       <Style.Intro>
-        <h1>
-          Pokemon n°{pokemon_number} : {pokemon}
-        </h1>
-        <img src={pokemon_image_url} alt={pokemon} />
+        <Pokemon name="Carapuce" id={7} />
+        <Pokemon name="Carabaffe" id={8} />
+        <Pokemon name="Tortank" id={9} />
       </Style.Intro>
     );
   }

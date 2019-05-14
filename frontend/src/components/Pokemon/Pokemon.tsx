e37e6567dda570c8af42decae921731797236c5d
Jérémy <jeremy.lauret@eleves.enpc.fr>
2019-05-14 13:36:12 +0200

    Capitalize Pokemon name.

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -8,6 +8,11 @@ interface Props {
 }
 
 class Pokemon extends React.Component<Props> {
+  // Return the Pokemon name with its first letter capitalized.
+  get capitalName(): string {
+    return `${this.props.name.charAt(0).toUpperCase()}${this.props.name.slice(1)}`;
+  }
+
   render(): React.ReactNode {
     const pokemonImageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
       this.props.id
@@ -16,9 +21,9 @@ class Pokemon extends React.Component<Props> {
     return (
       <Style.PokemonCard>
         <h1>
-          Pokemon n°{this.props.id} : {this.props.name}
+          Pokemon n°{this.props.id} : {this.capitalName}
         </h1>
-        <img src={pokemonImageUrl} alt={this.props.name} />
+        <img src={pokemonImageUrl} alt={this.capitalName} />
       </Style.PokemonCard>
     );
   }

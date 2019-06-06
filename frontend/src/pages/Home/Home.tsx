b7c316221e6912d5e5dcb1c0684b26d9933eba0c
JeremyLauret <jeremy.lauret@eleves.enpc.fr>
2019-06-06 14:14:30 +0200

    Turn Home into a functional component.

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -5,56 +5,52 @@ import Pokemon from 'components/Pokemon';
 import { makeGetRequest } from 'services/networking/request';
 
 interface Props {}
-interface State {
-  loading: boolean;
-  error: boolean;
-  pokemons: {
-    id: number;
-    name: string;
-    height: number;
-    weight: number;
-  }[];
-}
 
-class Home extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      loading: true,
-      error: false,
-      pokemons: [],
-    };
-  }
+function Home(props: Props): React.ReactNode {
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(false);
+  const [pokemons, setPokemons] = React.useState<
+    {
+      id: number;
+      name: string;
+      height: number;
+      weight: number;
+    }[]
+  >([]);
 
-  async componentDidMount() {
-    try {
-      let response = await makeGetRequest('/pokemon');
-      this.setState({ loading: false, pokemons: response.body });
-    } catch (e) {
-      this.setState({ loading: false, error: true });
+  React.useEffect(() => {
+    async function fillGrid() {
+      try {
+        let response = await makeGetRequest('/pokemon');
+        setPokemons(response.body);
+      } catch (e) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
     }
-  }
 
-  render(): React.ReactNode {
-    return (
-      <Style.Pokedex>
-        <h1 className="pokedex-title">Pokedex</h1>
-        {this.state.loading && <img src={`${process.env.PUBLIC_URL}/loader.svg`} alt="Loader" />}
-        {this.state.error && <p className="error-message">Something wrong happened. Send help.</p>}
-        <div className="pokedex-grid">
-          {this.state.pokemons.map(item => (
-            <Pokemon
-              name={item.name}
-              id={item.id}
-              height={item.height}
-              weight={item.weight}
-              key={item.id}
-            />
-          ))}
-        </div>
-      </Style.Pokedex>
-    );
-  }
+    fillGrid();
+  });
+
+  return (
+    <Style.Pokedex>
+      <h1 className="pokedex-title">Pokedex</h1>
+      {loading && <img src={`${process.env.PUBLIC_URL}/loader.svg`} alt="Loader" />}
+      {error && <p className="error-message">Something wrong happened. Send help.</p>}
+      <div className="pokedex-grid">
+        {pokemons.map(item => (
+          <Pokemon
+            name={item.name}
+            id={item.id}
+            height={item.height}
+            weight={item.weight}
+            key={item.id}
+          />
+        ))}
+      </div>
+    </Style.Pokedex>
+  );
 }
 
 export default Home;

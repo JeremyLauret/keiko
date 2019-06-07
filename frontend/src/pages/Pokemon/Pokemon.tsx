d84030794a853e94e4156f7c0846dfe18b0ccfcd
JeremyLauret <jeremy.lauret@eleves.enpc.fr>
2019-06-07 14:34:05 +0200

    Complete and style Pokemon page.

--- frontend/src/pages/Pokemon/Pokemon.tsx
+++ frontend/src/pages/Pokemon/Pokemon.tsx
@@ -4,6 +4,9 @@ import Style from './Pokemon.style';
 import { makeGetRequest } from 'services/networking/request';
 import { RouteComponentProps } from 'react-router-dom';
 
+// Return a string with a capital first letter.
+let capitalizeFirstLetter = (str: string) => `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+
 function Pokemon(props: RouteComponentProps<{ id: string }>) {
   const [loading, setLoading] = React.useState(true);
   const [error, setError] = React.useState(false);
@@ -34,8 +37,39 @@ function Pokemon(props: RouteComponentProps<{ id: string }>) {
       {loading && <img src={`${process.env.PUBLIC_URL}/loader.svg`} alt="Loader" />}
       {error && <p className="error-message">Something wrong happened. Send help.</p>}
       {!error && !loading && (
-        <div>
-          <p>Name: {pokemon.name}</p>
+        <div className="container">
+          <h1 className="main-title">{capitalizeFirstLetter(pokemon.name)}</h1>
+          <figure className="grid">
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                pokemon.id
+              }.png`}
+              alt={capitalizeFirstLetter(pokemon.name)}
+            />
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${
+                pokemon.id
+              }.png`}
+              alt={`${capitalizeFirstLetter(pokemon.name)} back`}
+            />
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${
+                pokemon.id
+              }.png`}
+              alt={`Shiny ${capitalizeFirstLetter(pokemon.name)}`}
+            />
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${
+                pokemon.id
+              }.png`}
+              alt={`Shiny ${capitalizeFirstLetter(pokemon.name)} back`}
+            />
+          </figure>
+          <div className="info">
+            <p className="card-info">Height: {pokemon.height}</p>
+            <p className="card-info">Weight: {pokemon.weight}</p>
+            <p className="card-info">Id: {pokemon.id}</p>
+          </div>
         </div>
       )}
     </Style.PokemonPage>

8e1dcf1c2b9f9c672d3eb5795f251376fc780907
JeremyLauret <jeremy.lauret@eleves.enpc.fr>
2019-06-11 12:20:45 +0200

    Initialize pokemon redux state by hand and display it with Home.

--- frontend/src/pages/Home/Home.wrap.ts
+++ frontend/src/pages/Home/Home.wrap.ts
@@ -1,9 +1,10 @@
-import { RouteComponentProps } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import withDataFetching from '../../HOC/withDataFetching';
-import { makeGetRequest } from 'services/networking/request';
 import Home from './Home';
+import { RootState } from '../../redux/types';
 
-export default withDataFetching('pokemons', (props: RouteComponentProps<{ page?: string }>) =>
-  makeGetRequest(`/pokemon${props.match.params.page ? `?page=${props.match.params.page}` : ''}`),
-)(Home);
+const mapStateToProps = (state: RootState) => ({
+  pokemons: Object.values(state.pokemon),
+});
+
+export default connect(mapStateToProps)(Home);

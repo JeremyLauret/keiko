d1b1ad757ef7cecb07d04b5436f646026d9f0af6
JeremyLauret <jeremy.lauret@eleves.enpc.fr>
2019-06-18 15:22:25 +0200

    Remove loader and error handling (should be dealt with later using redux).

--- frontend/src/HOC/withDataFetching.tsx
+++ frontend/src/HOC/withDataFetching.tsx
@@ -5,22 +5,16 @@ import Style from './withDataFetching.style';
 const withDataFetching = (fetchActionDispatcher: Function) => (
   BaseComponent: React.ComponentType<any>,
 ) => (props: any) => {
-  const [loading, setLoading] = React.useState(true);
-  const [error, setError] = React.useState(false);
-
   React.useEffect(
     () => {
       fetchActionDispatcher(props);
-      setLoading(false);
     },
     [props.match.params], // Re-render when URL parameters change.
   );
 
   return (
     <Style.DataFetching>
-      {loading && <img src={`${process.env.PUBLIC_URL}/loader.svg`} alt="Loader" />}
-      {error && <p className="error-message">Something wrong happened. Send help.</p>}
-      {!loading && !error && <BaseComponent {...props} />}
+      <BaseComponent {...props} />
     </Style.DataFetching>
   );
 };

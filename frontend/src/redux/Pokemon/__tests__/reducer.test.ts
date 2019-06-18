0dbb16bcbb888231fd5f0da1bfe70328c0b12710
JeremyLauret <jeremy.lauret@eleves.enpc.fr>
2019-06-18 15:11:02 +0200

    Fix variable name.

--- frontend/src/redux/Pokemon/__tests__/reducer.test.ts
+++ frontend/src/redux/Pokemon/__tests__/reducer.test.ts
@@ -6,13 +6,13 @@ const pokemons = {
   91: { id: 91, name: 'cloyster', height: 15, weight: 1325 },
   92: { id: 92, name: 'gastly', height: 13, weight: 1 },
 };
-const pokemon = pokemons[91];
+const cloyster = pokemons[91];
 
 describe('Pokemon reducer', () => {
   describe('FETCH_POKEMON_SUCCESS case', () => {
     it('Should return an initial state with a pokemon in its id field', () => {
-      const action = fetchPokemonSuccess(pokemon);
-      const expectedState = { ...initialState, [pokemon.id]: pokemon };
+      const action = fetchPokemonSuccess(cloyster);
+      const expectedState = { ...initialState, [cloyster.id]: cloyster };
 
       expect(reducer(initialState, action)).toEqual(expectedState);
     });
